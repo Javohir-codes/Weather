5917d7cc9f1663d474feb82a1fe911225e267a32
Javohir-codes <jo2011baxti@gmail.com>
2025-09-26 18:53:40 +0500

    js full

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const apiKey = "6806b8256e13875a96ddb6422a4b7e20";
 const city = "Tashkent"
-const nowtime = document.getElementById("now-time")
+const nowtime = document.getElementById("nowtime")
+const maindegree = document.getElementById("maindegree")
+const fellslike = document.getElementById("fellslike")
+const weathername = document.getElementById("weathername")
+const humidity = document.getElementById("humidity")
+const windspeed = document.getElementById("windspeed")
+const Pressure = document.getElementById("Pressure")
+const degree15 = document.getElementById("degree15")
+const degree18 = document.getElementById("degree18")
+const degree21 = document.getElementById("degree21")
+const speed15 = document.getElementById("speed15")
+const speed18 = document.getElementById("speed18")
+const speed21 = document.getElementById("speed21")
+const forecast5degree1 = document.getElementById("forecast5degree1")
+const forecast5degree2 = document.getElementById("forecast5degree2")
+const forecast5degree3 = document.getElementById("forecast5degree3")
+const forecast5degree4 = document.getElementById("forecast5degree4")
+const forecast5degree5 = document.getElementById("forecast5degree5")
+const forecast5data1 = document.getElementById("forecast5data1")
+const forecast5data2 = document.getElementById("forecast5data2")
+const forecast5data3 = document.getElementById("forecast5data3")
+const forecast5data4 = document.getElementById("forecast5data4")
+const forecast5data5 = document.getElementById("forecast5data5")
 
 async function getWeather() {
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
@@ -25,6 +47,28 @@ async function getWeather() {
         console.log(time)
         const todayForecast = data.list.filter(item => item.dt_txt.startsWith(today));
         nowtime.innerText = time
+        maindegree.innerText = `${todayForecast[0].main.temp}`
+        fellslike.innerText = `${todayForecast[0].main.feels_like}`
+        weathername.innerText = `${todayForecast[0].weather[0].main}`
+        Pressure.innerText = `${todayForecast[0].main.pressure}`
+        humidity.innerText = `${todayForecast[0].main.humidity}`
+        windspeed.innerText = `${todayForecast[0].wind.speed}`
+        degree15.innerText = `${todayForecast[0].main.temp}`
+        degree18.innerText = `${todayForecast[1].main.temp}`
+        degree21.innerText = `${todayForecast[2].main.temp}`
+        speed21.innerText = `${todayForecast[2].wind.speed}`
+        speed15.innerText = `${todayForecast[0].wind.speed}`
+        speed18.innerText = `${todayForecast[1].wind.speed}`
+        forecast5degree1.innerText = `${dailyForcast[0].main.temp}`
+        forecast5degree2.innerText = `${dailyForcast[1].main.temp}`
+        forecast5degree3.innerText = `${dailyForcast[2].main.temp}`
+        forecast5degree4.innerText = `${dailyForcast[3].main.temp}`
+        forecast5degree5.innerText = `${dailyForcast[4].main.temp}`
+        forecast5data1.innerText = `${dailyForcast[0].dt_txt}`
+        forecast5data2.innerText = `${dailyForcast[1].dt_txt}`
+        forecast5data3.innerText = `${dailyForcast[2].dt_txt}`
+        forecast5data4.innerText = `${dailyForcast[3].dt_txt}`
+        forecast5data5.innerText = `${dailyForcast[4].dt_txt}`
 
         console.log("Today's Hourly Forecast:", todayForecast);
 
